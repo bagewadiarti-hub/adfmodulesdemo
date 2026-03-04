2a80d874769168fdfdae1ee4e7d5161b57ec50c2
bagewadiarti-hub <bagewadi.arti@gmail.com>
2026-03-04 14:21:24 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,14 +1,10 @@
 terraform {
-  required_version = ">= 1.3.0"
-
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 4.0"
+      version = "~> 3.75"
     }
   }
-
-  backend "azurerm" {}
 }
 
 provider "azurerm" {
